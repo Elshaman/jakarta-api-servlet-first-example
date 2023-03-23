751763ba65e9d7e553f756783c89297b6f6a269a
ElShaman <cbuitragoo.2023@gmail.com>
2023-03-22 20:16:29 -0500

    primer servlet

---
+++ src/main/java/org/cbo/webapp/servlet/HolaMundoServlet.java
@@ -0,0 +1,24 @@
+package org.cbo.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("hola")
+public class HolaMundoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print();
+    }
+
+
+
+}
